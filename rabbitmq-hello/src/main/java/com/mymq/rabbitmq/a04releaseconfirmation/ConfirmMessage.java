d068e1b3d43606894ef715b84299dde9e0de225d
eniac555 <1150304664@qq.com>
2023-04-06 19:21:51 +0800

    处理异步未确认的消息

--- rabbitmq-hello/src/main/java/com/mymq/rabbitmq/a04releaseconfirmation/ConfirmMessage.java
+++ rabbitmq-hello/src/main/java/com/mymq/rabbitmq/a04releaseconfirmation/ConfirmMessage.java
@@ -5,6 +5,8 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.ConfirmCallback;
 
 import java.util.UUID;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 /**
  * 验证发布确认的不同模式
@@ -19,9 +21,9 @@ public class ConfirmMessage {
 
     public static void main(String[] args) throws Exception {
         //1.单个确认
-        //confirmForIndividual();
+        confirmForIndividual();
         //2.批量确认
-        //confirmForBatch();
+        confirmForBatch();
         //3.异步批量确认
         confirmForBatchAsync();
 
@@ -91,17 +93,34 @@ public class ConfirmMessage {
         channel.queueDeclare(queueName, true, false, false, null);
         //开启发布确认模式
         channel.confirmSelect();
+
+        //线程安全有序的一个哈希表，适用于高并发的情况下
+        //功能：
+        //1.轻松地将序号与消息关联
+        //2.可以方便的进行批量删除
+        //3.支持高并发（多线程）
+        ConcurrentSkipListMap<Long, String> outstandingConfirm = new ConcurrentSkipListMap<>();
+
         //开始时间
         long start = System.currentTimeMillis();
 
         //消息确认成功的回调函数
         ConfirmCallback ackCallback = (deliveryTag, multiple) -> {
             //参数1：消息的标记  参数2：是否批量确认
+            if (multiple) {
+                //1.2 删除掉已经成功的消息，剩下的就是未确认的消息
+                ConcurrentNavigableMap<Long, String> confirmed = outstandingConfirm.headMap(deliveryTag);
+                confirmed.clear();
+            } else {
+                outstandingConfirm.remove(deliveryTag);
+            }
             System.out.println("已经成功确认的消息" + deliveryTag);
         };
         //消息确认失败的回调函数
         ConfirmCallback nackCallback = (deliveryTag, multiple) -> {
-            System.out.println("未确认的消息" + deliveryTag);
+            //1.3 打印一下未确认的消息有哪些
+            String failMessage = outstandingConfirm.get(deliveryTag);
+            System.out.println("为确认的消息是：" + failMessage + ":::未确认的消息的标记是：" + deliveryTag);
         };
 
         //消息监听器，监听哪些消息成功，哪些消息失败
@@ -111,6 +130,13 @@ public class ConfirmMessage {
         for (int i = 0; i < MESSAGE_COUNT; i++) {
             String message = "" + i;
             channel.basicPublish("", queueName, null, message.getBytes());
+            //处理未成功确认的消息
+            //1.1 记录下所有要发送的消息
+            outstandingConfirm.put(channel.getNextPublishSeqNo(), message);
+            //1.2 删除掉已经成功的消息
+
+            //1.3 打印一下未确认的消息有哪些
+
         }
 
         //结束时间
