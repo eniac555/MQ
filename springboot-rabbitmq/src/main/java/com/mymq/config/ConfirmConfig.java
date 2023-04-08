0748cc993224fbe3cc3c963f5286b2896201f221
eniac555 <1150304664@qq.com>
2023-04-08 18:11:36 +0800

    备份交换机

--- springboot-rabbitmq/src/main/java/com/mymq/config/ConfirmConfig.java
+++ springboot-rabbitmq/src/main/java/com/mymq/config/ConfirmConfig.java
@@ -16,24 +16,80 @@ public class ConfirmConfig {
     public static final String CONFIRM_QUEUE_NAME = "confirm.queue";
     //路由key
     public static final String CONFIRM_ROUTING_KEY = "key1";
+    //备份交换机
+    public static final String BACKUP_EXCHANGE_NAME = "backup.exchange";
+    //备份队列
+    public static final String BACKUP_QUEUE_NAME = "backup.queue";
+    //报警队列
+    public static final String WARNING_QUEUE_NAME = "warning.queue";
+
 
     //交换机声明
     @Bean("confirmExchange")
     public DirectExchange confirmExchange() {
-        return new DirectExchange(CONFIRM_EXCHANGE_NAME);
+        return ExchangeBuilder.directExchange(CONFIRM_EXCHANGE_NAME)
+                .durable(true)
+                .withArgument("alternate-exchange", BACKUP_EXCHANGE_NAME)//把原有交换机和备份交换机进行关联
+                .build();
     }
 
     //队列声明
     @Bean("confirmQueue")
     public Queue confirmQueue() {
-        return QueueBuilder.durable(CONFIRM_QUEUE_NAME).build();
+        return QueueBuilder.durable(CONFIRM_QUEUE_NAME)
+                .build();
     }
 
     //绑定关系
     @Bean
     public Binding queueBindingExchange(@Qualifier("confirmQueue") Queue queue,
-                                        @Qualifier("confirmExchange") DirectExchange exchange){
-        return BindingBuilder.bind(queue).to(exchange).with(CONFIRM_ROUTING_KEY);
+                                        @Qualifier("confirmExchange") DirectExchange exchange) {
+        return BindingBuilder
+                .bind(queue)
+                .to(exchange)
+                .with(CONFIRM_ROUTING_KEY);
+    }
+
+
+
+
+    /*
+    =========================================================================================
+     */
+
+
+    //备份交换机声明 扇出
+    @Bean("backupExchange")
+    public FanoutExchange backupExchange() {
+        return new FanoutExchange(BACKUP_EXCHANGE_NAME);
+    }
+
+    //备份队列声明
+    @Bean("backupQueue")
+    public Queue backupQueue() {
+        return QueueBuilder.durable(BACKUP_QUEUE_NAME).build();
+    }
+
+
+    //报警队列声明
+    @Bean("warningQueue")
+    public Queue warningQueue() {
+        return QueueBuilder.durable(WARNING_QUEUE_NAME).build();
+    }
+
+    //绑定备份交换机和备份队列
+    @Bean
+    public Binding backupQueueBindingBackupExchange(@Qualifier("backupQueue") Queue backupQueue,
+                                                    @Qualifier("backupExchange") FanoutExchange backupExchange) {
+        return BindingBuilder.bind(backupQueue).to(backupExchange);
+    }
+
+
+    //绑定备份交换机和报警队列
+    @Bean
+    public Binding warningQueueBindingBackupExchange(@Qualifier("warningQueue") Queue warningQueue,
+                                                     @Qualifier("backupExchange") FanoutExchange backupExchange) {
+        return BindingBuilder.bind(warningQueue).to(backupExchange);
     }
 
 
